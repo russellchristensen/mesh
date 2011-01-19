1fbb4eda45c92516a3fc19c52fc7a2f3bccca92f
Nathan Stocks <nathan.stocks@gmail.com>
2011-01-18 17:31:02 -0700

    Updating all plugins to conform to the new plugin template I devised

--- src/p_svn.py
+++ src/p_svn.py
@@ -13,14 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Mesh.  If not, see <http://www.gnu.org/licenses/>.
 
-import meshlib, sys, time, unittest, zmq
+import meshlib, sys, unittest
 
 if __name__ == '__main__':
-   # Connect a PUSH socket to master.py
-   master_socket_url = sys.argv[1]
-   zmq_context       = zmq.Context()
-   push_master       = zmq_context.socket(zmq.PUSH)
-   push_master.connect(master_socket_url)
+   zmq_context, push_master = meshlib.init_plugin(sys.argv)
 
 supported_os = ['linux2']
 description = """
@@ -36,7 +32,7 @@ def configured():
       except: return False
    return True
 
-import subprocess, re
+import subprocess, re, time
 
 plugin_name = 'p_svn'
 parse = re.compile(r'^[\w ]+: (.+)[\n\r]?', re.MULTILINE)
